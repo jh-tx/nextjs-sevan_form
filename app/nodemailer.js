69f387e0f0d4741b8137ca314f45650e4c0ffd5e
jh-tx <jhtx.javascript@gmail.com>
2023-09-21 09:14:35 -0500

    updated email list

--- app/nodemailer.js
+++ app/nodemailer.js
@@ -2,6 +2,7 @@ import nodemailer from "nodemailer"
 
 const email = process.env.EMAIL
 const pass = process.env.EMAIL_PW
+const email_list = "thomas.cassity@sevansolutions.com, sevantest7@gmail.com"
 export const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
@@ -13,6 +14,6 @@ export const transporter = nodemailer.createTransport({
 
 export const mailOptions = {
     from:email,
-    to:email,
+    to:email_list,
 
 }
