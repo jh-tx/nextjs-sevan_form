5c50951015ac235fcc6da4710b80ece567ad5574
jh-tx <jhtx.javascript@gmail.com>
2023-09-21 00:29:21 -0500

    removed company logo

--- app/confirmation/page.js
+++ app/confirmation/page.js
@@ -6,7 +6,6 @@ export default function Page() {
             <p className="text-[100px] text-center">&#9989;</p>
             <p className='text-txt-green-color text-[30px] md:text-[30px] font-bold font-sans text-center'>THANK YOU!</p>
             <h6 className="text-center">Your submission has been sent.</h6>
-            <a href = 'https://www.sevansolutions.com/' target="_blank"><img src="https://www.sevansolutions.com/wp-content/uploads/2022/04/Sevan-logo.svg" alt="Sevan Logo" id="logo"></img></a>
         </div>
     )
 }
